5add6290ec6f3394dce4e36e46c789fb04944abb
Deepak Jagtap <deepakgithub4@gmail.com>
2026-07-05 22:12:03 +0530

    Update GetUsersTest.java

--- src/test/java/test/GetUsersTest.java
+++ src/test/java/test/GetUsersTest.java
@@ -7,9 +7,18 @@ public class GetUsersTest {
 	@Test
 	public void getUsers() {
 
+		// npm install -g json-server
+		// db.json file search on laptop its available at C:\Users\Deepak\db.json
+		// Start the server -> json-server --watch db.json
+		// Opne http://localhost:3000
+
 		baseURI = "http://localhost:3000";
 
-		given().when().get("/users").then().statusCode(200);
+		given().
+		when().
+		 get("/posts").
+		then().
+		 statusCode(200);
 
 	}
 }
